6f73f5c36dcc669370058cb25a0a28dcb76e95c6
O P Mitchell <43820040+OPMitchell@users.noreply.github.com>
2018-10-13 23:29:16 +0100

    Added CustomEditor script

---
+++ CustomEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (TerrainGenerator))]
+public class ProcTerrGUI : Editor 
+{
+	public override void OnInspectorGUI()
+	{
+		TerrainGenerator c = (TerrainGenerator)target;
+
+		DrawDefaultInspector ();
+		if (GUILayout.Button ("Generate Terrain")) 
+		{
+			c.Generate();
+		}
+	}
+}
