281fc9e44507fa2a1f2be5a1c187a1c7cd6f5f3e
O P Mitchell <43820040+OPMitchell@users.noreply.github.com>
2018-10-13 23:28:23 +0100

    Added TerrainGenerator script
    To be added as a component to an empty gameobject

---
+++ TerrainGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour 
+{
+	[SerializeField] int n;
+	[SerializeField] float spread;
+	[SerializeField] float spreadReductionRate;
+	[SerializeField] int seed;
+	[SerializeField] int height;
+	public void Generate() 
+	{
+		GameObject terrain = new GameObject("Terrain");
+		Terrain t = terrain.AddComponent<Terrain>();
+		TerrainData td = t.terrainData = new TerrainData();
+		td.heightmapResolution = (int)Mathf.Pow(2, n);
+		td.alphamapResolution = (int)Mathf.Pow(2, n);
+		t.heightmapPixelError = 0;
+		td.SetHeights(0, 0, MidpointDisplacement.CreateHeightmap(n, seed, spread, spreadReductionRate));
+		td.size = new Vector3(td.heightmapWidth, height, td.heightmapHeight);
+		terrain.AddComponent<TerrainCollider>();
+		GetComponent<TextureTerrain>().DoItBaby();
+	}
+}
